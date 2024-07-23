737b82f7d1cbdaeb3722525efa4e698c83b6b371
FacuDePaul <facundodepaul@gmail.com>
2024-07-23 17:10:30 -0300

    Revert "chore: Update react-navigation to version 5.0.0 and add @react-navigation/stack 6.4.1 dependency"
    This reverts commit 56e9b564232bbfc67ecd0535f29e1bcb58e20f94.

--- App.tsx
+++ App.tsx
@@ -6,23 +6,91 @@
  */
 
 import React, {useEffect} from 'react';
+import type {PropsWithChildren} from 'react';
 import {
+  Button,
+  FlatList,
   SafeAreaView,
+  ScrollView,
   StatusBar,
   StyleSheet,
+  Text,
   useColorScheme,
+  View,
 } from 'react-native';
 
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 import axios from 'axios';
-import RootStackNavigation from './navigation/RootStackNavigation';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+interface IMeliItem {
+  id: String;
+  title: String;
+}
+
+function Section({children, title}: SectionProps): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [list, setList] = React.useState([]);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  const getMeli = async (query: String) => {
+    return await axios.get(
+      `https://api.mercadolibre.com/sites/MLA/search?q=${query}&limit=3#json`,
+    );
+  };
+
+  const handleButtonPress = () => {
+    getMeli('iphone')
+      .then(res => {
+        console.log(res.data.results);
+        setList(res.data.results);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    console;
+  }, [list]);
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -30,7 +98,26 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <RootStackNavigation></RootStackNavigation>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Button title="Simple Button" onPress={handleButtonPress} />
+        </View>
+      </ScrollView>
+      <FlatList
+        data={list}
+        renderItem={({item}) => (
+          <Section title={item.title}>
+            <Text>id: {item.id}</Text>
+          </Section>
+        )}
+        keyExtractor={item => item.id.toString()}
+      />
     </SafeAreaView>
   );
 }
