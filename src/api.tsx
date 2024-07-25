27f1e680d3c5f8e69ceed525e9504b243c731c0d
FacuDePaul <facundodepaul@gmail.com>
2024-07-25 20:39:45 -0300

    .

--- src/api.tsx
+++ src/api.tsx
@@ -1,12 +1,22 @@
 import axios from 'axios';
 
 export type HomeQueryResponse = {
+  paging: {
+    total: number;
+    primary_results: number;
+    offset: number;
+    limit: number;
+  };
   results: Array<ProductQueryResponse>;
 };
 
-export const HomeFetchData = async (query: String, limit: number) =>
+export const HomeFetchData = async (
+  query: String,
+  limit: number,
+  offset: number,
+) =>
   await axios.get<HomeQueryResponse>(
-    `https://api.mercadolibre.com/sites/MLA/search?q=${query}&limit=${limit}#json`,
+    `https://api.mercadolibre.com/sites/MLA/search?q=${query}&limit=${limit}&offset=${offset}#json`,
   );
 
 export type ProductQueryResponse = {
